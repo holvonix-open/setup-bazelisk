05da2d27e57a04dfd52891a636579f8345540cc8
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2020-07-22 00:05:57 -0700

    v0.1.0 - Initial script to setup bazelisk

---
+++ README.md
@@ -0,0 +1,2 @@
+# setup-bazelisk
+GitHub action for setting up bazelisk (for Bazel builds)
---
+++ index.js
@@ -0,0 +1,36 @@
+const { exec } = require('child_process');
+
+const fs = require('fs');
+
+const pkgVer = process.env['INPUT_BAZELISKVERSION'] || 'latest';
+
+if (!/^[A-Z0-9a-z\-\+\.\^\~]+$/.test(pkgVer)) {
+  console.error(`Invalid version \`${pkgVer}\``);
+  process.exit(2);
+  return;
+}
+
+const licenseText = fs.readFileSync('LICENSE', {encoding: 'UTF-8'});
+
+console.log(licenseText);
+
+function execStrict(cmd, cb) {
+  exec(cmd, (error, stdout, stderr) => {
+    if (stderr) {
+      console.error(stderr);
+    }
+    console.log(stdout);
+    if (error) {
+      console.error(error.message);
+      process.exit(1);
+      return;
+    }
+    cb && cb();
+  });
+}
+
+execStrict(`npm i -g @bazel/bazelisk@${pkgVer}`, () => {
+  execStrict("which bazelisk && bazelisk --version", () => {
+    execStrict("which bazel && bazel --version");
+  });
+});
